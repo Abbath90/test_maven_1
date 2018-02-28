440a5470bc0abdd7b652c626a99bc4857afac14c
Abbath90 <khal.timur@gmail.com>
2018-02-28 19:20:30 +0300

    Hello world 2

--- src/main/java/com/app/HelloWorld.java
+++ src/main/java/com/app/HelloWorld.java
@@ -2,6 +2,6 @@ package com.app;
 
 public class HelloWorld {
     public static void main(String[] argv) {
-        System.out.println("Hello world");
+        System.out.println("Hello world 2");
     }
 }
